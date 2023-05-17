6de0c5156e56f558d167a55f807425a92a715071
vireakp <57161784+vireakp@users.noreply.github.com>
2023-05-17 22:48:17 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://seameoted.org/newsfeed/graphql` + encodeURI(path as string)
+					`https://seameoted.org/newsfeed/` + encodeURI(path as string)
 				}`,
 			},
 		};
